dd679adefae4f543572c98767761c93d19b23e59
iamblwb <leealbert@LeedeMacBook-Air.local>
2016-08-23 14:36:32 +0800

    Revert "add comment"
    This reverts commit 8a7e8217594982676163311dbd148a2279d74382.

--- app/src/main/java/com/example/leealbert/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/leealbert/myfirstapplication/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main)
         Log.d("testLog", "Hello");
-        //add comment
     }
 }
